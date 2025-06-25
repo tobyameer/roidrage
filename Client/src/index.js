1fdf4546d24acae6448d9a5400e178fb8dda2e21
Mohsen Ameer <mohsenameer15@gmail.com>
2025-06-24 23:30:56 -0400

    adding vercel

--- Client/src/index.js
+++ Client/src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
       <Routes>
+        <SpeedInsights />
         <App />
       </Routes>
     </AuthProvider>
